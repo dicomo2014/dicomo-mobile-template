9cd6c237e6cfec0ed0e66c3d39426b80517625d5
Nomura, Yoshihide <y.nomura@jp.fujitsu.com>
2014-05-21 21:12:36 +0900

    added generated sessions. added favorite function of sessions.

--- _scripts/gensession.rb
+++ _scripts/gensession.rb
@@ -5,6 +5,7 @@
 # Front matter付きのセッションページを生成
 
 require 'csv'
+require 'yaml'
 
 session_data_file = "_data/DICOMO2014_session_data.csv"
 session_folder = "_posts/session"
@@ -36,9 +37,48 @@ until reader.eof?
     puts
     next
   elsif /^(?<sid>\d[A-Z])\s(?<title>.+)/ =~ str then
-    sid.downcase!
-    session_file_name = sprintf "%d-%02d-%02d-session-%s.md", year, month, day, sid
+    # セッション情報
+    ssid = sid.downcase
+    session_file_name = sprintf "%d-%02d-%02d-session-%s.html", year, month, day, ssid
     puts session_file_name
+    session_file_name = session_folder + "/" + session_file_name
+    front = {"category" => "session", "layout" => "event"}
+    front["title"] = sid + " " + title
+    front["pageid"] = ssid
+    line = reader.shift
+    # 時間
+    if /^(?<sh>\d+):(?<sm>\d+)～(?<eh>\d+):(?<em>\d+)/ =~ line[0] then
+      front["start"] = sprintf "%d-%02d-%02d %02d:%02d:00", year, month, day, sh, sm
+      front["end"] = sprintf "%d-%02d-%02d %02d:%02d:00", year, month, day, eh, em
+    else
+      printf "session %s error", session_file_name
+      puts
+    end
+    File.open(session_file_name, "w") do |f|
+      f.write YAML.dump(front)
+      f.write "---\n"
+      # 座長
+      line = reader.shift
+      f.write line[0]
+      f.write "\n"
+      line = reader.shift # empty
+      while (line = reader.shift).length > 0 do
+        psid = line[0] # セッションサブ番号
+        ptime = line[1] # 時間
+        pid = line[2] # 論文番号
+        line = reader.shift # タイトル
+      f.write <<EOT
+<div data-role="collapsible">
+EOT
+        f.write "<h3>" + psid + ":" + line[0] + "</h3>\n"
+        f.write "<p>" + line[0] + "</p>\n"
+      f.write <<EOT
+</div>
+EOT
+        # 著者を読み飛ばす
+        line = reader.shift while line.length > 0
+      end
+    end
   end
 #  puts line
 end
